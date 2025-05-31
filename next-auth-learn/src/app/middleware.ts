396eaca90e9460672a14c19059573f16a44c71ee
guitadharsh <adharshd100@gmail.com>
2025-05-31 22:59:22 +0530

    fix: deleted old nextjs

--- next-auth-learn/src/app/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-import { authMiddleware } from "./middleware/auth";
-
-export function middlware(req: NextRequest) {
-    if(authMiddleware) return authMiddleware;
-
-    return NextResponse.next();
-}
